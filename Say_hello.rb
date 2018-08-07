a3d81a450565c30f5b96b55cfa8883f84aa657bd
Sarah Ng <agbtr101a@gmail.com>
2018-08-07 15:07:13 +0000

    automatically backed up by learn

--- Say_hello.rb
+++ Say_hello.rb
@@ -1,9 +1,9 @@
-def Say_hello(your_name)
+def Say_hello!(your_name)
   puts "Hello #{your_name}!"
 end
-Say_hello("Vivvy") 
-Say_hello("Lizzy")
-def Say_hello 
+Say_hello!("Vivvy") 
+Say_hello!("Lizzy")
+def Say_hello! 
   puts "Hello Ruby Programer!"
 end
-Say_hello 
+Say_hello! 
